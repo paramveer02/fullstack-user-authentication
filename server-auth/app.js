a16d318a81e7e53adef53b8e9d76045ad1ab2e84
Paramveer <paramveermarwahafc@gmail.com>
2025-09-14 11:49:35 +0200

    feat: handle global errors and invalid routes

--- server-auth/app.js
+++ server-auth/app.js
@@ -1,6 +1,9 @@
-import cookieParser from "cookie-parser";
 import express from "express";
 import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import notFound from "./middlewares/notFoundMiddleware.js";
+import errorHandler from "./middlewares/errorHandlerMiddleware.js";
+import { authRouter } from "./routes/authRouter.js";
 
 export const app = express();
 
@@ -9,6 +12,11 @@ app.use(helmet());
 app.use(express.json());
 app.use(cookieParser());
 
+app.use("/api/v1/auth", authRouter);
+
 app.get("/", (req, res) => {
   res.status(200).json({ status: "sucess" });
 });
+
+app.use(notFound);
+app.use(errorHandler);
