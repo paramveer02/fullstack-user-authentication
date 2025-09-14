16806148c0a3fb9103a6b10e51640169b217919e
Paramveer <paramveermarwahafc@gmail.com>
2025-09-14 15:50:43 +0200

    updated signup controller to handle duplicate email error

--- server-auth/controllers/authController.js
+++ server-auth/controllers/authController.js
@@ -2,11 +2,15 @@ import { StatusCodes } from "http-status-codes";
 import User from "../models/User.js";
 import { asyncWrapper } from "../utils/asyncWrapper.js";
 import { createSendToken } from "../utils/createSendToken.js";
+import { BadRequestError } from "../errors/customErrors.js";
 
 export const signup = asyncWrapper(async function (req, res) {
-  const { firstName, lastName, email, password } = req.body;
-  const user = await User.create({ firstName, lastName, email, password });
+  const { name, email, password } = req.body;
 
+  const exists = await User.findOne({ email });
+  if (exists) throw new BadRequestError("Email already exists");
+
+  const user = await User.create({ name, email, password, role: "user" });
   createSendToken(user, StatusCodes.CREATED, res);
 });
 
