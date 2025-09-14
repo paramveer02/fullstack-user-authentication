b4813b21aba83a74fa9d1f0933087bfc62e5a155
Paramveer <paramveermarwahafc@gmail.com>
2025-09-15 00:45:25 +0200

    feat: added login and logout handler methods

--- server-auth/controllers/authController.js
+++ server-auth/controllers/authController.js
@@ -2,7 +2,11 @@ import { StatusCodes } from "http-status-codes";
 import User from "../models/User.js";
 import { asyncWrapper } from "../utils/asyncWrapper.js";
 import { createSendToken } from "../utils/createSendToken.js";
-import { BadRequestError } from "../errors/customErrors.js";
+import {
+  BadRequestError,
+  NotFoundError,
+  UnauthenticatedError,
+} from "../errors/customErrors.js";
 
 export const signup = asyncWrapper(async function (req, res) {
   const { firstName, lastName, email, password } = req.body;
@@ -20,5 +24,29 @@ export const signup = asyncWrapper(async function (req, res) {
   createSendToken(user, StatusCodes.CREATED, res);
 });
 
-export const login = asyncWrapper(async function (req, res) {});
-export const logout = asyncWrapper(async function (req, res) {});
+export const login = asyncWrapper(async function (req, res) {
+  const { email, password } = req.body;
+
+  if (!email || !password)
+    throw new BadRequestError("Please provide your email and password");
+
+  const user = await User.findOne({ email: email }).select("+password");
+
+  if (!user || !(await user.correctPassword(password, user.password)))
+    throw new UnauthenticatedError("Incorrect Credentials");
+
+  createSendToken(user, StatusCodes.OK, res);
+});
+
+export const logout = asyncWrapper(async function (req, res) {
+  const isProd = process.env.NODE_ENV === "production";
+
+  res.clearCookie("token", {
+    httpOnly: true,
+    secure: isProd,
+    sameSite: isProd ? "none" : "lax",
+    path: "/",
+    // expires: new Date(0),
+  });
+  res.status(StatusCodes.OK).json({ message: "User logged out!" });
+});
