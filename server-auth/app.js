8aff855dbe13bf6a237951ef9106769bfb1611cc
Paramveer <paramveermarwahafc@gmail.com>
2025-09-14 15:47:26 +0200

    feat: added cors whitelist and config

--- server-auth/app.js
+++ server-auth/app.js
@@ -1,13 +1,35 @@
 import express from "express";
+import cors from "cors";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
-import notFound from "./middlewares/notFoundMiddleware.js";
-import errorHandler from "./middlewares/errorHandlerMiddleware.js";
+import notFound from "./middlewares/notFound.js";
+import errorHandler from "./middlewares/errorHandler.js";
 import { authRouter } from "./routes/authRouter.js";
 
 export const app = express();
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://127.0.0.1:5173",
+  "http://localhost:5174",
+  "http://127.0.0.1:5174",
+];
+
+app.use(
+  cors({
+    origin: (origin, cb) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        return cb(null, true);
+      } else {
+        cb(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
+
 app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 
 app.use(express.json());
 app.use(cookieParser());
