e4308f098b10ab58429163bd0be33d4edb7d0ae3
Paramveer <paramveermarwahafc@gmail.com>
2025-09-14 15:47:51 +0200

    updated invalid route and global error handler middlewares

--- server-auth/middlewares/errorHandler.js
+++ server-auth/middlewares/errorHandler.js
@@ -4,6 +4,13 @@ const errorHandler = (err, req, res, next) => {
   const statusCode = err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR;
   const message = err.message || "Something went wrong";
 
+  if (err.name === "ValidationError") {
+    statusCode = StatusCodes.BAD_REQUEST;
+    message = Object.values(err.errors)
+      .map((e) => e.message)
+      .join(", ");
+  }
+
   return res.status(statusCode).json({ message });
 };
 
