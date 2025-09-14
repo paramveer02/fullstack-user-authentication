eda3168fa068da350f85b4afcb741ac10861bbc7
Paramveer <paramveermarwahafc@gmail.com>
2025-09-14 11:51:50 +0200

    feat: added User model

---
+++ server-auth/models/User.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: [true, "Please provide your first name."],
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    required: [true, "Please provide your last name."],
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide your email."],
+    unique: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide a password."],
+    minlength: 8,
+    select: false,
+  },
+  role: {
+    type: String,
+    enum: ["user", "admin"],
+    default: "user",
+  },
+});
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 12);
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
