9ba47ecd547706974e99e29dbd8b7dbd3db07f42
Paramveer <paramveermarwahafc@gmail.com>
2025-09-15 17:40:25 +0200

    feat: added protected routes: profile, about

--- client-auth/src/App.jsx
+++ client-auth/src/App.jsx
@@ -4,7 +4,11 @@ import Error from "./pages/Error";
 
 import SignUp, { action as signupAction } from "./pages/authentication/SignUp";
 import Login, { action as loginAction } from "./pages/authentication/Login";
-import Dashboard from "./pages/Dashboard";
+import DashboardLayout from "./pages/DashboardLayout";
+import { loader as dashboardLoader } from "./pages/DashboardLayout";
+import { action as dashboardAction } from "./pages/DashboardLayout";
+import Profile from "./pages/Profile";
+import About from "./pages/About";
 
 const router = createBrowserRouter([
   {
@@ -12,9 +16,23 @@ const router = createBrowserRouter([
     element: <HomeLayout />,
     errorElement: <Error />,
     children: [
-      { path: "signup", element: <SignUp />, action: signupAction },
+      {
+        index: true,
+        element: <SignUp />,
+        action: signupAction,
+      },
       { path: "login", element: <Login />, action: loginAction },
-      { path: "dashboard", element: <Dashboard /> },
+      {
+        id: "dashboard",
+        path: "dashboard",
+        element: <DashboardLayout />,
+        loader: dashboardLoader,
+        action: dashboardAction,
+        children: [
+          { path: "profile", element: <Profile /> },
+          { path: "about", element: <About /> },
+        ],
+      },
     ],
   },
 ]);
