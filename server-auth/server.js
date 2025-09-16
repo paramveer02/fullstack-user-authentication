4ce4d61f9b8d73c724aeed4d8f892561687dee85
Paramveer <paramveermarwahafc@gmail.com>
2025-09-16 13:29:31 +0200

    feat: added cloudinary config for storing images

--- server-auth/server.js
+++ server-auth/server.js
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 import { app } from "./app.js";
 import "./config/db.js";
+import cloudinary from "cloudinary";
 
 import morgan from "morgan";
 
@@ -11,6 +12,12 @@ const PORT = process.env.PORT || 5100;
 if (process.env.NODE_ENV !== "production") {
   app.use(morgan("dev"));
 }
+
+cloudinary.v2.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
+});
 
 app.listen(PORT, () =>
   console.log(
