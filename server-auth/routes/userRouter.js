90aaa0239d868c9ff40c8891f43bf37f1f89bebe
Paramveer <paramveermarwahafc@gmail.com>
2025-09-16 13:26:46 +0200

    feat: added protected update user route

--- server-auth/routes/userRouter.js
+++ server-auth/routes/userRouter.js
@@ -1,3 +1,8 @@
 import Router from "express";
+import { updateMe } from "../controllers/userController.js";
+import { protect } from "../middlewares/auth.js";
+import upload from "../middlewares/multer.js";
 
 export const userRouter = Router();
+
+userRouter.patch("/update/me", protect, upload, updateMe);
